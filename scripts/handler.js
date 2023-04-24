ed29c427bbc3375158a1e4df0451d3ed94062d20
DerzeTTTT <u13_davdro_lbn@technischools.com>
2023-04-24 18:13:19 +0200

    login n stuff

--- scripts/handler.js
+++ scripts/handler.js
@@ -2,15 +2,21 @@
 const openLogin = document.querySelector('#open-login');
 const loginBox = document.querySelector('#login-box');
 
-const [username, password] = document.querySelector('.username'), document.querySelector('.password');
+const loginButton = document.querySelector('#login-button')
+
+const usernameField = document.querySelector('.username');
+const passwordField = document.querySelector('.password');
+
 
 //Variables
 let webData = new Object();
 webData.loginOpen = false;
 
+webData.splitter = 'okwow';
+
 
 //Functions
-const toggleLogin = () => {
+const toggleLoginBox = () => {
 
     webData.loginOpen = !webData.loginOpen;
 
@@ -20,12 +26,73 @@ const toggleLogin = () => {
 
 };
 
-const loginUser = () => {
+const getCredentials = () => {
 
+    const [username, password] = [usernameField.value, passwordField.value];
 
+    if (username === '' || password === ''){
+
+        alert('Username/Password can\'t be empty!');  
+        return;
+
+    };
+
+    toggleLoginBox();
+
+    usernameField.value = '';
+    passwordField.value = '';
+
+    return [username, password];
+
+};
+
+const loginUser = (reloadCallback) => {
+
+    const [username, password] = getCredentials();
+
+    loginToken = `${btoa(username)}${webData.splitter}${btoa(password)}`;
+    document.cookie = `loginSession=${loginToken}`;
+
+    webData.username = username;
+    webData.password = password;
+
+};
+
+const getCookie = (name) => {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+const digestCookie = () => {
+
+    loginSess = getCookie('loginSession');
+    if (!loginSess){return};
+
+    splitted = loginSess.split(webData.splitter);
+
+    const [username, password] = [atob(splitted[0]), atob(splitted[1])];
+
+    webData.username = username;
+    webData.password = password;
+
+    console.log(username, password);
+
+};
+
+const submitFile = () => {
+
+    
 
 };
 
 
 //Events
-openLogin.addEventListener('click', toggleLogin);
+openLogin.addEventListener('click', toggleLoginBox);
+loginButton.addEventListener('click', () => {loginUser(); location.reload()});
+
+
+//Misc
+loginBox.style.display = 'none';
+
+digestCookie();
