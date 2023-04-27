7ec3eb44a04834bce2dcb6f032190d2f4e9fadb1
DerzeTTTT <u13_davdro_lbn@technischools.com>
2023-04-27 16:20:14 +0200

    finishing

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ const http = require('http');
 const sha256 = require('sha256');
 const url = require('url');
 const queryString = require('querystring');
+const formidable = require('formidable');
+const fs = require('fs');
+const admZip = require('adm-zip');
 
 const serverPort = 55405;
 
@@ -21,6 +24,8 @@ class credentials {
         const checkQuery = `SELECT * FROM [Users] WHERE Usernm='${this.username}'` + (withPassword ? ` AND Passwd='${this.hashed}'` : '');
         const queryResults = await queryDB(checkQuery);
 
+        console.log(queryResults);
+
         return queryResults.length !== 0;
 
     };
@@ -31,6 +36,17 @@ class credentials {
         const queryResults = await queryDB(registerQuery);
 
         return queryResults
+
+    };
+
+    async getID(){
+
+        const idQuery = `SELECT TOP(1) Id FROM [Users] WHERE Usernm='${this.username}'`;
+        const queryResults = await queryDB(idQuery);
+
+        console.log(queryResults, idQuery);
+
+        return queryResults[0]['Id'];
 
     };
     
@@ -46,13 +62,16 @@ const config = {
     }
 };
 
+const uploadDir = `${__dirname.replace(/\\/g, '/')}/uploaded`;
 
 //functions
 const queryDB = (tQuery) => {
 
     return sql.connect(config)
         .then(pool => {
+
             return pool.request().query(tQuery);
+            
         })
 
         .then(result => {
@@ -115,6 +134,127 @@ apiMethods.POST['register'] = async (req, res, params) => {
 
 };
 
+apiMethods.POST['upload'] = (req, res) => {
+
+    let form = new formidable.IncomingForm();
+
+    form.parse(req, async function (err, fields, files) {
+
+        const newCredentials = new credentials(fields.username, fields.password);4
+
+        if(!await newCredentials.validate()){
+
+            console.log('Invalid credentials')
+            res.end();
+
+            return;
+
+        };
+
+        const oldPath = files.file.filepath;
+        const userId = await newCredentials.getID();
+
+        const holderPath = `${uploadDir}/${userId}`;
+
+        if (!fs.existsSync(holderPath)){
+
+            fs.mkdirSync(holderPath);
+            console.log('created', holderPath);
+
+        };
+        
+        fs.rename(oldPath, `${holderPath}/${files.file.originalFilename}`, (err) => {
+
+            if(err) throw err;
+
+            res.write('Uploaded');
+            res.end();
+
+        });
+
+    });
+
+};
+
+const downloadFile = async function(filePath, res){
+
+    console.log("Reading", filePath);
+
+    const rs = fs.createReadStream(filePath);
+
+    rs.pipe(res);
+
+};
+
+apiMethods.GET['retrieveAll'] = async (req, res, params) => {
+
+    const username = params.username;
+    const password = params.password;
+
+    const newCredentials = new credentials(username, password);
+
+    if(!await newCredentials.validate()){
+
+        console.log('Invalid credentials')
+        res.end();
+
+        return;
+
+    };
+
+    const userId = await newCredentials.getID();
+    const retrievingFolder = `${uploadDir}/${userId}`;
+
+    console.log(retrievingFolder);
+
+    if(!fs.existsSync(retrievingFolder)){
+
+        res.writeHead(500);
+        res.end();
+
+        return;
+
+    };
+
+    fs.readdir(retrievingFolder, async (err, files) => {
+
+        if (err){
+
+            console.error(err);
+
+            res.writeHead(500);
+            res.end();
+
+            return;
+
+        };
+
+        const zipFileName = `retrievedFiles.zip`;
+        const newZip = new admZip();
+
+        files.forEach((readingFile) => {
+
+            const filePath = `${retrievingFolder}/${readingFile}`;
+            console.log(filePath);
+
+            newZip.addLocalFile(filePath);
+
+        });
+        
+        const zipContent = newZip.toBuffer();
+
+        res.setHeader("Content-Length", Buffer.byteLength(zipContent));
+        res.setHeader("Content-Type", "text/plain");
+        res.setHeader("Content-Disposition", `attachment; filename=${zipFileName}`);
+
+        res.end(zipContent);
+
+        fs.rmSync(retrievingFolder, {recursive: true, force: true});
+
+    });
+
+};  
+
 const requestListener = (req, res) => {
 
     res.setHeader('Content-Type', 'application/json');
@@ -129,7 +269,7 @@ const requestListener = (req, res) => {
 
     if (tMethod && tMethod[endpoint]){
 
-        res.writeHead(200);
+        //res.writeHead(200);
         tMethod[endpoint](req, res, queryParams);
 
     } else {
--- scripts/handler.js
+++ scripts/handler.js
@@ -13,11 +13,27 @@ const passwordField = document.querySelector('.password');
 
 const incorrectInfo = document.querySelector('.incorrect');
 
+const alreadyLogged = document.querySelector('.already-logged');
+
+const alreadyLoggedText = document.querySelector('.already-logged span');
+const logOut = document.querySelector('.log-out');
+
+const uploadForm = document.querySelector('.upload-form');
+const fileInput = document.querySelector('.browse-file');
+
+const retrieveAll = document.querySelector('.retrieve');
+
 //Objects
 const user = new Object();
 const design = new Object();
 
 design.loginDisplayed = false;
+
+user.loggedIn = false;
+user.cookieSplitter = 'okwow';
+
+user.username = '';
+user.password = '';
 
 //Functions
 design.toggleLogin = function(){
@@ -36,12 +52,12 @@ design.displayIncorrect = function(msg){
 
 user.readData = function(){
 
-    user.username = usernameField.value;
-    user.password = passwordField.value;
+    const username = usernameField.value;
+    const password = passwordField.value;
 
     console.log(usernameField.value)
 
-    if(user.username === '' || user.password === ''){
+    if(username === '' || password === ''){
 
         design.displayIncorrect("Username and password can't be empty!");
 
@@ -49,11 +65,11 @@ user.readData = function(){
 
     };
 
-    return true;
+    return [username, password];
 
 };
 
-const sendRequest = async function(sendingUrl, method){
+const sendRequest = async function(sendingUrl, method, convertToJSON){
 
     const response = await fetch(`${backendServer}/${sendingUrl}`, {
 
@@ -61,7 +77,7 @@ const sendRequest = async function(sendingUrl, method){
 
     });
 
-    return await response.json();
+    return await convertToJSON ? response : response.json();
 
 };
 
@@ -70,19 +86,22 @@ user.register = async function(){
     const validatedFields = user.readData();
     if(!validatedFields){return};
 
-    const takenUsername = await sendRequest(`validateUsername?username=${user.username}`, 'GET');
-    if(takenUsername===1){
+    const username = validatedFields[0];
+    const password = validatedFields[1];
+
+    const takenUsername = await sendRequest(`validateUsername?username=${username}`, 'GET');
+    if(takenUsername==1){
 
         design.displayIncorrect("Username already taken");
         return;
 
     };
 
-    const registerStatus = await sendRequest(`register?username=${user.username}&password=${user.password}`, 'POST');
+    const registerStatus = await sendRequest(`register?username=${username}&password=${password}`, 'POST');
 
-    if (registerStatus===1){
+    if (registerStatus==1){
 
-        alert(`Successfully created account ${user.username}`);
+        alert(`Successfully created account ${username}`);
 
     } else {
 
@@ -90,20 +109,176 @@ user.register = async function(){
 
     };
 
-};
-
-user.login = function(){
-
-
+    await user.login(username, password);
+    if(user.loggedIn){location.reload()};
 
 };
+
+user.getCookie = async function(username, password){
+
+    const gotCookie = `${btoa(username)}${user.cookieSplitter}${btoa(password)}`;
+    return gotCookie;
+
+};
+
+async function readCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+};
+
+user.digestCookie = async function(){
+
+    const foundCookie = await readCookie('loginSession');
+    if(!foundCookie || foundCookie==''){document.cookie='loginSession='; return};
+
+    const splitted = foundCookie.split(user.cookieSplitter);
+    const [username, password] = [atob(splitted[0]), atob(splitted[1])];
+
+    await user.login(username, password);
+    
+};
+
+user.login = async function(username, password){
+
+    const checkedCredentials = await sendRequest(`validate?username=${username}&password=${password}`, 'GET');
+    if(checkedCredentials==0){
+
+        design.displayIncorrect('Incorrect username or password');
+        return;
+
+    };
+
+    const settingCookie = await user.getCookie(username, password);
+    document.cookie = `loginSession=${settingCookie}`;
+
+    console.log('Logging in as', username);
+
+    loginBox.style.display = 'none';
+
+    alreadyLoggedText.innerText = `Logged in as ${username}`;
+    alreadyLogged.style.display = 'flex'
+
+    user.loggedIn = true;
+
+    user.username = username;
+    user.password = password;
+
+};
+
+user.logOut = async function(){
+
+    document.cookie = 'loginSession=';
+    location.reload();
+
+};
+
+user.handleLogin = async function(){
+
+    const validatedFields = user.readData();
+    if(!validatedFields){return};
+
+    await user.login(validatedFields[0], validatedFields[1]);
+    if(user.loggedIn){location.reload()};
+
+};
+
+user.sendFile = async function(sendingFile){
+
+    const xhr = new XMLHttpRequest();
+    const formData = new FormData();
+
+    formData.append('file', sendingFile);
+
+    formData.append('username', user.username);
+    formData.append('password', user.password);
+
+    xhr.open('POST', `http://localhost:${backendPort}/upload`, true);
+
+    xhr.onload = () => {
+
+        alert(xhr.status == 200 ? 'Successfully uploaded file' : 'Failed to upload file');
+
+    };
+
+    xhr.send(formData);
+
+    console.log('Sent file');
+
+};
+
+// user.retrieveFiles = async function(){
+
+//     const resp = await sendRequest(`retrieveAll?username=${user.username}&password=${user.password}`, 'GET');
+//     const blob = await resp.blob();
+    
+//     const downloadLink = document.createElement('a');
+
+//     downloadLink.href = URL.createObjectURL(blob);
+//     downloadLink.download = 'retrievedFiles.zip';
+
+//     document.body.appendChild(downloadLink);
+
+//     downloadLink.click();
+
+//     document.body.removeChild(downloadLink);
+
+// };
+
+user.retrieveFiles = async function() {
+
+    const resp = await sendRequest(`retrieveAll?username=${user.username}&password=${user.password}`, 'GET', true);
+
+    if(resp.status == 500){
+
+        alert("Failed to retrieve files! Perhaps you didn't upload any?");
+        return;
+        
+    };
+
+    const blob = await resp.blob();
+
+    const downloadLink = document.createElement('a');
+
+    downloadLink.href = URL.createObjectURL(blob);
+    downloadLink.download = 'retrievedFiles.zip';
+
+    document.body.appendChild(downloadLink);
+
+    downloadLink.click();
+
+    document.body.removeChild(downloadLink);
+
+};
+  
 
 //Events
 openLogin.addEventListener('click', design.toggleLogin);
 
 registerButton.addEventListener('click', user.register);
-loginButton.addEventListener('click', user.login);
+loginButton.addEventListener('click', user.handleLogin);
+
+logOut.addEventListener('click', user.logOut);
+
+uploadForm.addEventListener('submit', (event) => {
+
+    if(!user.loggedIn){alert('You need to be logged in to send files!'); return};
+
+    event.preventDefault();
+
+    const sendingFile = fileInput.files[0];
+
+    user.sendFile(sendingFile);
+
+});
+
+retrieveAll.addEventListener('click', user.retrieveFiles);
 
 //Misc
 loginBox.style.display = 'none';
 incorrectInfo.style.display = 'none';
+
+user.digestCookie();
+
+usernameField.value = '';
+passwordField.value = '';
